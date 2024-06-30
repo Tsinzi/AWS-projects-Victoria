8e8c0d36da54839b367b3dc06e436dfeb443796e
Tsinzi <vickykabajwara@gmail.com>
2024-06-30 17:44:16 +0100

    Update hello-world-app.py

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -10,4 +10,4 @@ def forth(id):
     return f"ID of this page is{id}"
 if __name__ == '__main__':
      #app.run(debug=True, port=30000)
-     app.run(host= '0.0.0.0', port=8080)
+     app.run(host= '0.0.0.0', port=8081)
